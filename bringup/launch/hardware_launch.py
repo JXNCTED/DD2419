dc4f1341fe957296eebc0aec41d4440296e9e5cc
NUC <you@example.com>
2024-02-04 17:56:22 +0100

    add bringup launch

---
+++ bringup/launch/hardware_launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='controller',
+            executable='cartesian_controller'
+        ) # chassis cartetian controller
+    ])
