2448f2afe11bb01b4f8e592407859ded7ab5cf47
NUC <you@example.com>
2024-02-04 21:32:50 +0100

    change PID a bit

--- controller/controller/cartesian_controller.py
+++ controller/controller/cartesian_controller.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Twist
 
 from math import pi
 
+
 class CartesianController(Node):
     def __init__(self):
         super().__init__('cartesian_controller')
@@ -16,29 +17,43 @@ class CartesianController(Node):
         self.int_error_wheel1 = 0.0
         self.int_error_wheel2 = 0.0
 
-        self.twist_sub_ = self.create_subscription(Twist, '/motor_controller/twist', self.twist_callback, 10)
-        self.encoder_sub_ = self.create_subscription(Encoders, '/motor/encoders', self.encoder_callback, 10)
-        self.duty_cycle_pub_ = self.create_publisher(DutyCycles, '/motor/duty_cycles', 10)
-    
+        self.last_delta_left = 0.0
+        self.last_delta_right = 0.0
+
+        self.twist_sub_ = self.create_subscription(
+            Twist, '/motor_controller/twist', self.twist_callback, 10)
+        self.encoder_sub_ = self.create_subscription(
+            Encoders, '/motor/encoders', self.encoder_callback, 10)
+        self.duty_cycle_pub_ = self.create_publisher(
+            DutyCycles, '/motor/duty_cycles', 10)
+
     def twist_callback(self, msg):
         self.target_v = msg.linear.x
         self.target_w = msg.angular.z
-    
+
     def encoder_callback(self, msg):
         TICK_PER_REV = 3600
         WHEEL_RADIUS = 0.0352
         WHEEL_BASE = 0.23
 
-        KP = 0.8
-        KI = 0.2
+        KP = 0.5
+        KI = 0.8
 
         DT = 50 / 1000
+        ALPHA = 0.5
 
-        vw1 = (msg.delta_encoder_left * 2 * pi) / TICK_PER_REV * WHEEL_RADIUS / DT
-        vw2 = (msg.delta_encoder_right * 2 * pi) / TICK_PER_REV * WHEEL_RADIUS / DT
+        self.last_delta_left = msg.delta_encoder_left * \
+            ALPHA + self.last_delta_left * (1 - ALPHA)
+        self.last_delta_right = msg.delta_encoder_right * \
+            ALPHA + self.last_delta_right * (1 - ALPHA)
 
-        v = (vw1 + vw2) / 2
-        w = (vw1 - vw2) / WHEEL_BASE
+        vw1 = (self.last_delta_left * 2 * pi) / \
+            TICK_PER_REV * WHEEL_RADIUS / DT
+        vw2 = (self.last_delta_right * 2 * pi) / \
+            TICK_PER_REV * WHEEL_RADIUS / DT
+
+        # v = (vw1 + vw2) / 2
+        # w = (vw1 - vw2) / WHEEL_BASE
 
         target_wheel1 = (self.target_v - self.target_w * WHEEL_BASE)
         target_wheel2 = (self.target_v + self.target_w * WHEEL_BASE)
@@ -49,13 +64,15 @@ class CartesianController(Node):
         self.int_error_wheel1 += error_wheel1 * DT
         self.int_error_wheel2 += error_wheel2 * DT
 
-        self.get_logger().info(f"int1: {self.int_error_wheel1}, int2:{self.int_error_wheel2}")
+        # self.get_logger().info(f"int1: {self.int_error_wheel1}, int2:{self.int_error_wheel2}")
 
-        self.int_error_wheel1 = min(max(self.int_error_wheel1, -0.3), 0.3)
-        self.int_error_wheel2 = min(max(self.int_error_wheel2, -0.3), 0.3)
+        self.int_error_wheel1 = min(max(self.int_error_wheel1, -0.1), 0.1)
+        self.int_error_wheel2 = min(max(self.int_error_wheel2, -0.1), 0.1)
 
-        pwm_1 = min(max(KP * error_wheel1 + KI * self.int_error_wheel1, -1.0), 1.0)
-        pwm_2 = min(max(KP * error_wheel2 + KI * self.int_error_wheel2, -1.0), 1.0)
+        pwm_1 = min(max(KP * error_wheel1 + KI *
+                    self.int_error_wheel1, -1.0), 1.0)
+        pwm_2 = min(max(KP * error_wheel2 + KI *
+                    self.int_error_wheel2, -1.0), 1.0)
 
         pub_msg = DutyCycles()
         pub_msg.duty_cycle_left = pwm_1
@@ -72,8 +89,6 @@ def main():
     except KeyboardInterrupt:
         pass
     rclpy.shutdown()
-
-
 
 
 if __name__ == '__main__':
