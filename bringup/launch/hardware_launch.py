3a6b86bf51ebd2f1ed299a45d0d4fe79c2f82b37
NUC <you@example.com>
2024-02-04 21:32:39 +0100

    update bringup

--- bringup/launch/hardware_launch.py
+++ bringup/launch/hardware_launch.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import launch
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
 
 
 def generate_launch_description():
@@ -33,26 +35,61 @@ def generate_launch_description():
         ],
         output='both',
     )  # all phidgets node
+    realsense_path = get_package_share_directory('realsense2_camera')
+    realsense_launch = launch.actions.IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(
+        realsense_path, 'launch/rs_launch.py')), launch_arguments={'pointcloud.enable': 'true'}.items())
+
+    bringup_path = get_package_share_directory('bringup')
 
     return launch.LaunchDescription([
+        # phidgets
         phidgets_container,
+
+        # joy
         Node(
             package='joy', executable='joy_node', name='joy_node',
             parameters=[{
                 'deadzone': 0.3,
                 'autorepeat_rate': 20.0,
             }]),
-        Node(
-            package='controller',
-            executable='cartesian_controller'
-        ),  # chassis cartetian controller
-        Node(
-            package='chassis_controller',
-            executable='joy_controller'
-        ),
+
+        # chassis controller
+
+        Node(package='controller',
+             executable='cartesian_controller',
+             ),
+
+        # servo arm
         Node(
             package='micro_ros_agent',
             executable='micro_ros_agent',
             arguments=["serial", "--dev", "/dev/ttyUSB0", "-v6"]
         ),
+
+        # realsense
+        realsense_launch,
+        Node(executable='static_transform_publisher', package='tf2_ros', arguments=[
+            '--child-frame-id', 'camera_link', '--frame-id', 'base_link']),
+
+        # lidar
+        Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB1',
+                'serial_baudrate': 115200,
+                'frame_id': 'lidar_link',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        ),
+        Node(executable='static_transform_publisher', package='tf2_ros', arguments=[
+            '--child-frame-id', 'lidar_link', '--frame-id', 'base_link']),
+
+        # camera on arm
+        Node(package='usb_cam',
+             executable='usb_cam_node_exe',
+             parameters=[os.path.join(bringup_path, 'config/usb_cam_param.yaml')]),
     ])
