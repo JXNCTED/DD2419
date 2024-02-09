1f52e23adbc534236ed9f3db3c9863b4c9fa4cc3
NUC <you@example.com>
2024-02-09 13:26:23 +0100

    track visual feature

--- odometry/src/vi_odom.cpp
+++ odometry/src/vi_odom.cpp
@@ -1,10 +1,50 @@
-#include <cstdio>
+#include <cv_bridge/cv_bridge.h>
 
+#include "opencv4/opencv2/opencv.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "string"
+#include "vector"
+
+void drawImage(const cv::Mat &img, const std::vector<cv::Point2f> &pts, const std::string name)
+{
+    auto draw = img.clone();
+    for (auto pt : pts)
+    {
+        cv::circle(draw, pt, 2, cv::Scalar(255, 0, 0), -1, 8);
+    }
+
+    cv::imshow(name, draw);
+    cv::waitKey(1);
+}
+class VisualOdom : public rclcpp::Node
+{
+   public:
+    VisualOdom() : Node("visual_odom")
+    {
+        image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/camera/color/image_raw", 10, std::bind(&VisualOdom::imgCallback, this, std::placeholders::_1));
+    }
+
+    void imgCallback(const sensor_msgs::msg::Image::SharedPtr msg)
+    {
+        static std::vector<cv::Point2f> nPts;
+
+        cv_bridge::CvImageConstPtr ptr;
+        ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+
+        cv::goodFeaturesToTrack(ptr->image, nPts, 100, 0.01, 12.0, cv::Mat(), 3, false, 0.04);
+        drawImage(ptr->image, nPts, "feature");
+    }
+
+   private:
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+};
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<VisualOdom>());
 
-    printf("hello world odometry package\n");
+    rclcpp::shutdown();
     return 0;
 }
