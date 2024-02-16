702a1e7a4ae654c19372e648cc0602b93df4fee6
Adam Ekelöf <adameke@kth.se>
2024-02-16 16:34:02 +0100

    move functionality from wheel_odom to filter_odom

--- odometry/src/filter_odom.cpp
+++ odometry/src/filter_odom.cpp
@@ -1,39 +1,50 @@
-#include "sensor_msgs/msg/imu.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "tf2/LinearMath/Quaternion.h"
+#include "rclcpp/rclcpp.hpp" //general
+#include "sensor_msgs/msg/imu.hpp" //imu
+#include "geometry_msgs/msg/vector3.hpp" //wheels
+#include "tf2/LinearMath/Quaternion.h" //odometry, path and tf
+#include "nav_msgs/msg/odometry.hpp" //odometry
+#include "geometry_msgs/msg/pose_stamped.hpp" //path
+#include "nav_msgs/msg/path.hpp" //path
+#include "tf2_ros/transform_broadcaster.h" //tf
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp" //tf
 
 class FilterOdom : public rclcpp::Node {
 public:
     FilterOdom() : 
         node("filter_odom")
     {
-        wheel_odom_sub_     = this->create_subscription<geometry_msgs::msg::Twist>("", 10, 
+        wheel_odom_sub_     = this->create_subscription<geometry_msgs::msg::Vector3>("/wheel_odom", 10, 
             std::bind(&FilterOdom::wheel_callback, this, std::placeholders::_1));
         realsense_gyro_sub_ = this->create_subscription<sensor_msgs::msg::Imu>("/camera/gyro/sample", 10, 
             std::bind(&FilterOdom::realsense_callback, this, std::placeholders::_1));
         phidget_gyro_sub_   = this->create_subscription<sensor_msgs::msg::Imu>("/imu/data_raw", 10, 
             std::bind(&FilterOdom::phidget_callback, this, std::placeholders::_1));
         odom_pub_           = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
+        path_pub_           = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
+        tf_broadcaster_     = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
         timer_ = this->create_wall_timer(50ms,
             std::bind(&FilterOdom::timer_callback, this));
     }
     
     void timer_callback() {
+        rclcpp::Time stamp = realsense_stamp_;
+        double wheels[2] = wheel_odom;
+        double realsense[2] = realsense_odom;
         double dt = 50.0/1000.0;
-        double avg_lin = (wheel_odom[0] + realsense_odom[0])/2.0;
-        double avg_ang = (wheel_odom[1] + realsense_odom[1])/2.0;
+        double avg_lin = (wheels[0] + realsense[0])/2.0;
+        double avg_ang = (wheels[1] + realsense[1])/2.0;
         x_ += avg_lin * sin(yaw_) * dt;
         y_ += avg_lin * cos(yaw_) * dt;
         yaw_ += avg_ang * dt;
-        pubilsh_odom();
+        pubilsh_odom(stamp);
+        publish_path(stamp);
+        broadcast_tf(stamp);
     }
 private: 
-    void publish_odom()
+    void publish_odom(const rclcpp::Time & stamp)
     {
         nav_msgs::msg::Odometry odom;
-        odom.header.stamp          = realsense_stamp_;
+        odom.header.stamp          = stamp;
         odom.header.frame_id       = "odom";
         odom.pose.pose.position.x  = x_;
         odom.pose.pose.position.y  = y_;
@@ -43,10 +54,37 @@ private:
 
         odom_pub_->publish(odom);
     }
- 
-    void wheel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
-        wheel_odom[0] = msg->linear.x;
-        wheel_odom[1] = msg->angular.z;
+    void publish_path(const rclcpp::Time &stamp)
+    {
+        geometry_msgs::msg::PoseStamped pose;
+        pose.header.stamp     = stamp;
+        pose.header.frame_id  = "odom";
+        pose.pose.position.x  = x;
+        pose.pose.position.y  = y;
+        pose.pose.position.z  = 0.0;
+        tf2::Quaternion q     = tf2::Quaternion(tf2::Vector3(0, 0, 1), yaw);
+        pose.pose.orientation = tf2::toMsg(q);
+        path_.header.stamp    = stamp;
+        path_.header.frame_id = "odom";
+        path_.poses.push_back(pose);
+        path_pub_->publish(path_);
+    }
+    void broadcast_tf(const rclcpp::Time &stamp)
+    {
+        geometry_msgs::msg::TransformStamped odom_tf;
+        odom_tf.header.stamp            = stamp;
+        odom_tf.header.frame_id         = "odom";
+        odom_tf.child_frame_id          = "base_link";
+        odom_tf.transform.translation.x = x_;
+        odom_tf.transform.translation.y = y_;
+        odom_tf.transform.translation.z = 0.0;
+        tf2::Quaternion q               = tf2::Quaternion(tf2::Vector3(0, 0, 1), yaw);
+        odom_tf.transform.rotation      = tf2::toMsg(q);
+        tf_broadcaster_->sendTransform(odom_tf);
+    }
+    void wheel_callback(const geometry_msgs::msg::Vector3::SharedPtr msg) {
+        wheel_odom[0] = msg->x;
+        wheel_odom[1] = msg->y;
     }
     void realsense_callback(const sensor_msgs::msg::Imu::SharedPtr msg) {
         realsense_odom[0] = (msg->linear_acceleration.z + _realsense_old[0])/2.0; // ---------------- I dont know if this is the correct axis
@@ -59,19 +97,27 @@ private:
         phidget_odom[0] = msg->linear_acceleration.x // ---------------- I dont know if this is the correct axis
         phidget_odom[1] = msg->angular_velocity.z;
     }
+
+    // Pubs and subs
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr realsense_gyro_sub_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr phidget_gyro_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr wheel_odom_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr wheel_odom_sub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+    nav_msgs::msg::Path path_;
+    // Fields
     double wheel_odom[2];
     double _realsense_old[2];
     double realsense_odom[2];
     double phidget_odom[2];
+    
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Time realsense_stamp_;
+
     float x_   = 0.0;
     float y_   = 0.0;
     float yaw_ = 0.0;
-    rclcpp::Time realsense_stamp_;
 }
 
 int main(int argc, char **argv) {
--- odometry/src/wheel_odom.cpp
+++ odometry/src/wheel_odom.cpp
@@ -1,73 +1,23 @@
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "robp_interfaces/msg/encoders.hpp"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "tf2_ros/transform_broadcaster.h"
 
 class WheelOdom : public rclcpp::Node
 {
    public:
     WheelOdom() : Node("wheel_odom")
     {
-        path_pub_    = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
-        odom_pub_    = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
+        odom_pub_    = this->create_publisher<geometry_msgs::msg::Vector3>("/wheel_odom", 10);
         encoder_sub_ = this->create_subscription<robp_interfaces::msg::Encoders>(
             "/motor/encoders", 10, std::bind(&WheelOdom::encodersCallback, this, std::placeholders::_1));
-        tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     }
 
    private:
-    void publish_path(const rclcpp::Time &stamp, const float &x, const float &y, const float &yaw)
-    {
-        geometry_msgs::msg::PoseStamped pose;
-        pose.header.stamp     = stamp;
-        pose.header.frame_id  = "odom";
-        pose.pose.position.x  = x;
-        pose.pose.position.y  = y;
-        pose.pose.position.z  = 0.0;
-        tf2::Quaternion q     = tf2::Quaternion(tf2::Vector3(0, 0, 1), yaw);
-        pose.pose.orientation = tf2::toMsg(q);
-        path_.header.stamp    = stamp;
-        path_.header.frame_id = "odom";
-        path_.poses.push_back(pose);
-        path_pub_->publish(path_);
-    }
-    void publish_odom(const rclcpp::Time &stamp, const float &x, const float &y, const float &yaw)
-    {
-        nav_msgs::msg::Odometry odom;
-        odom.header.stamp          = stamp;
-        odom.header.frame_id       = "odom";
-        odom.pose.pose.position.x  = x;
-        odom.pose.pose.position.y  = y;
-        odom.pose.pose.position.z  = 0.0;
-        tf2::Quaternion q          = tf2::Quaternion(tf2::Vector3(0, 0, 1), yaw);
-        odom.pose.pose.orientation = tf2::toMsg(q);
-
-        odom_pub_->publish(odom);
-    }
-    void broadcast_tf(const rclcpp::Time &stamp, const float &x, const float &y, const float &yaw)
-    {
-        geometry_msgs::msg::TransformStamped odom_tf;
-        odom_tf.header.stamp            = stamp;
-        odom_tf.header.frame_id         = "odom";
-        odom_tf.child_frame_id          = "base_link";
-        odom_tf.transform.translation.x = x;
-        odom_tf.transform.translation.y = y;
-        odom_tf.transform.translation.z = 0.0;
-        tf2::Quaternion q               = tf2::Quaternion(tf2::Vector3(0, 0, 1), yaw);
-        odom_tf.transform.rotation      = tf2::toMsg(q);
-        tf_broadcaster_->sendTransform(odom_tf);
-    }
     void encodersCallback(const robp_interfaces::msg::Encoders::SharedPtr msg)
     {
         const float TICK_PER_REV = 3600;
         const float WHEEL_RADIUS = 0.04921;
         const float WHEEL_BASE   = 0.3;
-
-        const float DT = 50.0 / 1000.0;
 
         float vw1 = msg->delta_encoder_left * 2 * M_PI / TICK_PER_REV / DT;
         float vw2 = msg->delta_encoder_right * 2 * M_PI / TICK_PER_REV / DT;
@@ -75,24 +25,14 @@ class WheelOdom : public rclcpp::Node
         float v = (vw1 + vw2) * WHEEL_RADIUS / 2;
         float w = (vw2 - vw1) * WHEEL_RADIUS / WHEEL_BASE;
 
-        x_ += v * DT * cos(yaw_);
-        y_ += v * DT * sin(yaw_);
-        yaw_ += w * DT;
-
-        publish_path(msg->header.stamp, x_, y_, yaw_);
-        publish_odom(msg->header.stamp, x_, y_, yaw_);
-        broadcast_tf(msg->header.stamp, x_, y_, yaw_);
+        geometry_msgs::msg::Vector3 odom;
+        odom.x = v;
+        odom.y = w;
+        odom_pub_->publish(odom);
     }
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+
+    rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr odom_pub_;
     rclcpp::Subscription<robp_interfaces::msg::Encoders>::SharedPtr encoder_sub_;
-    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-
-    nav_msgs::msg::Path path_;
-
-    float x_   = 0.0;
-    float y_   = 0.0;
-    float yaw_ = 0.0;
 };
 
 int main(int argc, char **argv)
