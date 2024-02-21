ae095d2832569d06d7c69e1ec01abaa65254c2b5
Thomas <birchler@kth.se>
2024-02-21 18:09:24 +0100

    added first todos

--- workspace/workspace/workspace.py
+++ workspace/workspace/workspace.py
@@ -1,5 +1,47 @@
+#!/usr/bin/env python
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Point
+
+
+class Workspace(Node):
+
+    def __init__(self):
+        super().__init__('open_loop_controller')
+        
+        # TODO: Listener: Receives (Nav-)point
+        self.nav_point_subscription = self.create_subscription(
+            Point, '/nav_point/point', self.nav_point_callback, 10)
+        # TODO: Publisher: Tells if received point is in workspace
+        # TODO: Publisher: Broadcast workspace to RVIZ (for visualization) 
+        
+    # TODO: Function?: Hard-Code workspace
+    def workspace_boundaries(self):
+        #Line 1
+        line1 
+        pass
+    
+    # TODO: Function: Check if point is in workspace
+    
+
+
+    def nav_point_callback(msg):
+        # Call isInWorkspace(msg)
+        # publish answer
+        pass
+
+
 def main():
-    print('Hi from workspace.')
+    rclpy.init()
+    node = Workspace()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
