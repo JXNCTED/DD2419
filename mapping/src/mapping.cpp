a4bc85a33d433a997c596f419e8d5d4da59c19ec
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-02-22 12:48:35 +0100

    save map every 5s

--- mapping/src/mapping.cpp
+++ mapping/src/mapping.cpp
@@ -6,56 +6,72 @@
 #include "tf2/LinearMath/Matrix3x3.h"
 class MappingNode : public rclcpp::Node
 {
-public:
-  MappingNode() : Node("mapping"), map(0.05, 1000, 1000, 500, 500), mapper(&map)
-  {
-    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/odom", 10, std::bind(&MappingNode::odomCallback, this, std::placeholders::_1));
+   public:
+    MappingNode()
+        : Node("mapping"), map(0.05, 1000, 1000, 500, 500), mapper(&map)
+    {
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom",
+            10,
+            std::bind(&MappingNode::odomCallback, this, std::placeholders::_1));
 
-    laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        "/scan", 10, std::bind(&MappingNode::laserCallback, this, std::placeholders::_1));
+        laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "/scan",
+            10,
+            std::bind(
+                &MappingNode::laserCallback, this, std::placeholders::_1));
 
-    occu_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/occupancy", 10);
-  }
+        occu_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(
+            "/occupancy", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(5000),
+            std::bind(&MappingNode::timerCallback, this));
+    }
 
-  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
-  {
-    pose.x = msg->pose.pose.position.x;
-    pose.y = msg->pose.pose.position.y;
-    tf2::Quaternion q(
-        msg->pose.pose.orientation.x,
-        msg->pose.pose.orientation.y,
-        msg->pose.pose.orientation.z,
-        msg->pose.pose.orientation.w);
-    tf2::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-    pose.theta = yaw;
-  }
+    void timerCallback()
+    {
+        static int count = 0;
+        map.saveMap("~/mappings/" + std::to_string(count) + "map.txt");
+    }
 
-  void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-  {
-    mapper.updateMapLaser(msg, pose);
-    nav_msgs::msg::OccupancyGrid occu;
-    occu = map.toRosOccGrid("odom");
-    occu_pub_->publish(occu);
-  }
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        pose.x = msg->pose.pose.position.x;
+        pose.y = msg->pose.pose.position.y;
+        tf2::Quaternion q(msg->pose.pose.orientation.x,
+                          msg->pose.pose.orientation.y,
+                          msg->pose.pose.orientation.z,
+                          msg->pose.pose.orientation.w);
+        tf2::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+        pose.theta = yaw;
+    }
 
-private:
-  GridMap map;
-  Mapper mapper;
-  Pose pose;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
-  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occu_pub_;
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        mapper.updateMapLaser(msg, pose);
+        nav_msgs::msg::OccupancyGrid occu;
+        occu = map.toRosOccGrid("odom");
+        occu_pub_->publish(occu);
+    }
+
+   private:
+    GridMap map;
+    Mapper mapper;
+    Pose pose;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occu_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-  rclcpp::spin(std::make_shared<MappingNode>());
+    rclcpp::spin(std::make_shared<MappingNode>());
 
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::shutdown();
+    return 0;
 }
