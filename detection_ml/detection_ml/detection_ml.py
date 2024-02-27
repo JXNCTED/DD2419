2d119ddba1bd09b72cf71025158f6b779ad2539f
NUC <you@example.com>
2024-02-27 20:02:37 +0100

    init detectin ml

--- detection_ml/detection_ml/detection_ml.py
+++ detection_ml/detection_ml/detection_ml.py
@@ -1,5 +1,34 @@
+#!/usr/bin/env python
+import torch
+import cv_bridge
+from dd2419_detector_baseline.detector import Detector
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+
+class DetectionMLNode(Node):
+    def __init__(self):
+        super().__init__('arm_controller')
+        self.model = Detector().eval().to("cuda")
+        self.model.load_state_dict(torch.load(
+            "dd2419_detector_baseline/best.pt"))
+
+        self.image_sub = self.create_subscription(
+            Image, "/palceholder", self.img_callback)
+
+    def img_callback(self, msg: Image):
+        pass
+
+
 def main():
-    print('Hi from detection_ml.')
+    rclpy.init()
+    node = DetectionMLNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
