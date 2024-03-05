aa2cab919214bfa857975cb82a58f930bded1c34
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-03-05 11:02:32 +0100

    delete unwanted code

--- controller/controller/cartesian_controller.py
+++ controller/controller/cartesian_controller.py
@@ -27,16 +27,9 @@ class CartesianController(Node):
         self.duty_cycle_pub_ = self.create_publisher(
             DutyCycles, '/motor/duty_cycles', 10)
 
-        # self.timeout_timer = self.create_timer(0.5, self.timeout_callback)
-
     def twist_callback(self, msg):
-        # self.timeout_timer.reset()
         self.target_v = msg.linear.x
         self.target_w = msg.angular.z
-
-    # def timeout_callback(self):
-    #     self.target_v = 0
-    #     self.target_w = 0
 
     def encoder_callback(self, msg):
         TICK_PER_REV = 3600
@@ -59,9 +52,6 @@ class CartesianController(Node):
         vw2 = (self.last_delta_right * 2 * pi) / \
             TICK_PER_REV * WHEEL_RADIUS / DT
 
-        # v = (vw1 + vw2) / 2
-        # w = (vw1 - vw2) / WHEEL_BASE
-
         target_wheel1 = (self.target_v - self.target_w * WHEEL_BASE)
         target_wheel2 = (self.target_v + self.target_w * WHEEL_BASE)
 
@@ -78,9 +68,6 @@ class CartesianController(Node):
                     self.int_error_wheel1, -1.0), 1.0)
         pwm_2 = min(max(KP * error_wheel2 + KI *
                     self.int_error_wheel2, -1.0), 1.0)
-
-        # self.get_logger().info(f"pwm: {pwm_1} {pwm_2}")
-        # self.get_logger().info(f"error: {error_wheel1} {error_wheel2}")
 
         pub_msg = DutyCycles()
         pub_msg.duty_cycle_left = pwm_1
