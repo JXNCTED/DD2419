258009b9ef2ac6ed6addb4c89b39921cf1f83094
NUC <you@example.com>
2024-03-26 11:54:10 +0100

    Built on the behavior tree, getting g00d

--- brain/brain/behaviors.py
+++ brain/brain/behaviors.py
@@ -8,6 +8,7 @@ import py_trees as pt
 import py_trees_ros as ptr
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 
 
 class ExploreBehavior(pt.behaviour.Behaviour):
@@ -36,8 +37,8 @@ class ApproachObjectBehavior(pt.behaviour.Behaviour):
         # place holder for the approach object behavior
         return pt.common.Status.SUCCESS
 
-    
-#Publishes message to move arm into detect mode. 
+
+# Publishes message to move arm into detect mode.
 class GraspObjectBehavior(pt.behaviour.Behaviour, Node):
     def __init__(self, name="GraspObjectBehavior"):
         # super(GraspObjectBehavior, self).__init__(name=name)
@@ -50,53 +51,79 @@ class GraspObjectBehavior(pt.behaviour.Behaviour, Node):
         # place holder for the grasp object behavior
         self.publisher_.publish(String(data="detect"))
         return pt.common.Status.SUCCESS
-    
-    
-  #This is the new class based on the code from above? 
-  class MoveArmIntoDetect(pt.behaviour.Behaviour, Node):
+
+
+class CloseGripperObjectBehavior(pt.behaviour.Behaviour, Node):
+    def __init__(self, name="CloseGripperObjectBehavior"):
+        # super(GraspObjectBehavior, self).__init__(name=name)
+        pt.behaviour.Behaviour.__init__(self, name=name)
+        Node.__init__(self, node_name=name)
+        self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
+        # pubclisher
+
+    def update(self):
+        # place holder for the grasp object behavior
+        self.publisher_.publish(String(data="close"))
+        return pt.common.Status.SUCCESS
+  # This is the new class based on the code from above?
+
+
+class MoveArmIntoDetect(pt.behaviour.Behaviour, Node):
     def __init__(self, name="GraspObjectBehavior"):
         # super(GraspObjectBehavior, self).__init__(name=name)
         pt.behaviour.Behaviour.__init__(self, name=name)
         Node.__init__(self, node_name=name)
         self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
-        self.publisherActivateDetect = self.create_publisher(Bool, '/can_detect',10)
+        # self.publisherActivateDetect = self.create_publisher(
+        # Bool, '/can_detect', 10)
         # pubclisher
 
     def update(self):
         # place holder for the grasp object behavior
         self.publisher_.publish(String(data="detect"))
-        self.publisherActivatedetect.publish(Bool(date=true))
+        # self.publisherActivateDetect.publish(Bool(date=True))
         return pt.common.Status.SUCCESS
-    
-    
-   class MoveArmInto(pt.behaviour.Behaviour, Node):
-    def __init__(self, name="GraspObjectBehavior"):
+
+
+# Moving arm into neutral position, can possibly change the other class to accomate different positions. Have to think..
+class MoveArmIntoNeutral(pt.behaviour.Behaviour, Node):
+    def __init__(self, name="MoveArmNeutral"):
         # super(GraspObjectBehavior, self).__init__(name=name)
         pt.behaviour.Behaviour.__init__(self, name=name)
         Node.__init__(self, node_name=name)
         self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
-        self.publisherActivateDetect = self.create_publisher(Bool, '/can_detect',10)
-        # pubclisher
 
     def update(self):
         # place holder for the grasp object behavior
-        self.publisher_.publish(String(data="detect"))
-        self.publisherActivatedetect.publish(Bool(date=true))
+        self.publisher_.publish(String(data="neutral"))
         return pt.common.Status.SUCCESS
-    
-   #Basic functionality from MS2 for the arm in the behavior tree.
+
+
+class MoveArmIntoPickUp(pt.behaviour.Behaviour, Node):
+    def __init__(self, name="MoveArmPickup"):
+        # super(GraspObjectBehavior, self).__init__(name=name)
+        pt.behaviour.Behaviour.__init__(self, name=name)
+        Node.__init__(self, node_name=name)
+        self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
+
+    def update(self):
+        # place holder for the grasp object behavior
+        self.publisher_.publish(String(data="pickup"))
+        return pt.common.Status.SUCCESS
+
+   # Basic functionality from MS2 for the arm in the behavior tree.
    # Sequence-->
-       # 1. Listen to topic if object is within 25cm, if so. return SUCCESS. 
-       # 2. PUBLISH to topic /arm_conf to put arm into detect, .. also PUBLISH to detect topic to allow arm camera to detect (With appropriate filter applied), return SUCCESS. 
-       # 3. Using the node to detect and move. If object not in the middle, return RUNNING and keep adjusting robot position. If object is centered, stop moving and return SUCCESS. 
-       
+       # 1. Listen to topic if object is within 25cm, if so. return SUCCESS.
+       # 2. PUBLISH to topic /arm_conf to put arm into detect, .. also PUBLISH to detect topic to allow arm camera to detect (With appropriate filter applied), return SUCCESS.
+       # 3. Using the node to detect and move. If object not in the middle, return RUNNING and keep adjusting robot position. If object is centered, stop moving and return SUCCESS.
+
        # SEQUENCE PICK UP -->
-           # 4. PUBLISH to /arm_conf to PICK UP. Return SUCCESS
-           # 5. Node that returns SUCCESS after some ticks.
-           # 6. PUBLISH to /arm_conf to NEUTRAL. Return SUCCESS. 
-            # SELECTOR FOR CHECKING IF PICKED UP? 
-        
-        
+        # 4. PUBLISH to /arm_conf to PICK UP. Return SUCCESS
+        # 5. Node that returns SUCCESS after some ticks.
+        # 6. PUBLISH to /arm_conf to NEUTRAL. Return SUCCESS.
+        # SELECTOR FOR CHECKING IF PICKED UP?
+
+
 class PlaceObjectBehavior(pt.behaviour.Behaviour):
     def __init__(self, name="PlaceObjectBehavior"):
         super(PlaceObjectBehavior, self).__init__(name=name)
--- brain/brain/tree.py
+++ brain/brain/tree.py
@@ -14,8 +14,8 @@ from .behaviors import *
 class BehaviorTree(ptr.trees.BehaviourTree):
     def __init__(self, unicode_tree_debug=False):
         self.root = pt.composites.Sequence("MainTree", memory=True)
-        
-        #Checking to see if object is within 25cm
+
+        # Checking to see if object is within 25cm
         getDist = ptr.subscribers.CheckData(
             name="getDist?",
             topic_name="/dist_bool",
@@ -24,22 +24,37 @@ class BehaviorTree(ptr.trees.BehaviourTree):
             qos_profile=ptr.utilities.qos_profile_unlatched(),
             expected_value=True,
         )
-        
-        #Checking if object is in the middle of the frame.
+
+        # Checking if object is in the middle of the frame.
         getMiddlePos = ptr.subscribers.CheckData(
             name="isInMiddleOfFrame?",
-            topic_name="/is_in_middle",
+            topic_name="/is_object_centered",
             topic_type=Bool,
             variable_name="data",
             qos_profile=ptr.utilities.qos_profile_unlatched(),
             expected_value=True,
         )
-        self.pickupSequence = pt.composites.Sequence("MainTree", memory=True)
-        self.pickupSequence.add_children([])
 
+        # -------------------Counter to slow down the loop-------------------
+        counter_fivethousand = pt.behaviours.TickCounter(
+            name="TickCounter", duration=10, completion_status=pt.common.Status.SUCCESS)
+        # -------------------Counter to slow down the loop-------------------
+
+        self.pickupSequence = pt.composites.Sequence("MainTree", memory=True)
+
+        self.pickupSequence.add_children([  # Pickup sequence
+            MoveArmIntoPickUp(),  # moving into pickup
+            counter_fivethousand,  # counter to slow down the loop
+            CloseGripperObjectBehavior(),
+            counter_fivethousand,  # counter to slow down the loop
+            MoveArmIntoNeutral(),  # moving into neutral
+        ])
 
         self.root.add_children([
             getDist,
+            MoveArmIntoDetect(),  # Something off here?
+            getMiddlePos,
+            self.pickupSequence,
             ExploreBehavior(),
             PickAndPlaceSelector(),
         ])
