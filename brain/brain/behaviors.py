4b990e1fd6d90011e19782928ad94e3c8172c010
Måns Wenning <manswenning@gmail.com>
2024-03-26 11:04:17 +0100

    Update behaviors.py

--- brain/brain/behaviors.py
+++ brain/brain/behaviors.py
@@ -36,7 +36,8 @@ class ApproachObjectBehavior(pt.behaviour.Behaviour):
         # place holder for the approach object behavior
         return pt.common.Status.SUCCESS
 
-
+    
+#Publishes message to move arm into detect mode. 
 class GraspObjectBehavior(pt.behaviour.Behaviour, Node):
     def __init__(self, name="GraspObjectBehavior"):
         # super(GraspObjectBehavior, self).__init__(name=name)
@@ -49,8 +50,53 @@ class GraspObjectBehavior(pt.behaviour.Behaviour, Node):
         # place holder for the grasp object behavior
         self.publisher_.publish(String(data="detect"))
         return pt.common.Status.SUCCESS
+    
+    
+  #This is the new class based on the code from above? 
+  class MoveArmIntoDetect(pt.behaviour.Behaviour, Node):
+    def __init__(self, name="GraspObjectBehavior"):
+        # super(GraspObjectBehavior, self).__init__(name=name)
+        pt.behaviour.Behaviour.__init__(self, name=name)
+        Node.__init__(self, node_name=name)
+        self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
+        self.publisherActivateDetect = self.create_publisher(Bool, '/can_detect',10)
+        # pubclisher
 
+    def update(self):
+        # place holder for the grasp object behavior
+        self.publisher_.publish(String(data="detect"))
+        self.publisherActivatedetect.publish(Bool(date=true))
+        return pt.common.Status.SUCCESS
+    
+    
+   class MoveArmInto(pt.behaviour.Behaviour, Node):
+    def __init__(self, name="GraspObjectBehavior"):
+        # super(GraspObjectBehavior, self).__init__(name=name)
+        pt.behaviour.Behaviour.__init__(self, name=name)
+        Node.__init__(self, node_name=name)
+        self.publisher_ = self.create_publisher(String, '/arm_conf', 10)
+        self.publisherActivateDetect = self.create_publisher(Bool, '/can_detect',10)
+        # pubclisher
 
+    def update(self):
+        # place holder for the grasp object behavior
+        self.publisher_.publish(String(data="detect"))
+        self.publisherActivatedetect.publish(Bool(date=true))
+        return pt.common.Status.SUCCESS
+    
+   #Basic functionality from MS2 for the arm in the behavior tree.
+   # Sequence-->
+       # 1. Listen to topic if object is within 25cm, if so. return SUCCESS. 
+       # 2. PUBLISH to topic /arm_conf to put arm into detect, .. also PUBLISH to detect topic to allow arm camera to detect (With appropriate filter applied), return SUCCESS. 
+       # 3. Using the node to detect and move. If object not in the middle, return RUNNING and keep adjusting robot position. If object is centered, stop moving and return SUCCESS. 
+       
+       # SEQUENCE PICK UP -->
+           # 4. PUBLISH to /arm_conf to PICK UP. Return SUCCESS
+           # 5. Node that returns SUCCESS after some ticks.
+           # 6. PUBLISH to /arm_conf to NEUTRAL. Return SUCCESS. 
+            # SELECTOR FOR CHECKING IF PICKED UP? 
+        
+        
 class PlaceObjectBehavior(pt.behaviour.Behaviour):
     def __init__(self, name="PlaceObjectBehavior"):
         super(PlaceObjectBehavior, self).__init__(name=name)
