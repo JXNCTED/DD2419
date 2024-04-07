23c0a4afd362331fb1ee0fb24756659c7775b612
NUC <you@example.com>
2024-04-07 22:42:14 +0200

    play with contours, looks good

--- arm/arm/arm_test_detect.py
+++ arm/arm/arm_test_detect.py
@@ -10,6 +10,15 @@ from setuptools import setup
 class ImageFilterNode(Node):
     def __init__(self):
         super().__init__('image_filter_node')
+
+        # hardcoded camera parameters
+        self.K = np.array([[513.34301, 0., 307.89617],
+                           [0., 513.84807, 244.62007],
+                           [0., 0., 1.]])
+
+        self.coeffs = np.array(
+            [-0.474424, 0.207336, -0.002361, 0.000427, 0.000000])
+
         self.subscription = self.create_subscription(
             Image,
             '/image_raw',
@@ -28,30 +37,40 @@ class ImageFilterNode(Node):
         )
 
     def image_callback(self, msg):
-        # Perform image filtering to find the center
-        # Add your filtering logic here
-        self.get_logger().info('Image received')
+        # self.get_logger().info('Image received')
+
         img = CvBridge().imgmsg_to_cv2(msg, 'bgr8')
-        h, w, c = img.shape
-        lower_res = img.copy()
-        # lower_res = cv.pyrDown(img, dstsize=(w // 2, h // 2))
-        cropimg = lower_res[0:int(lower_res.shape[0]*0.8)]
-        blurimg = cv.GaussianBlur(cropimg, (5, 5), 0)
-        lab = cv.cvtColor(blurimg, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(lab, (0, 50, 120), (255, 255, 255))
-        res = cv.bitwise_and(blurimg, blurimg, mask=mask)
-        self.publisher.publish(CvBridge().cv2_to_imgmsg(res, 'bgr8'))
-        edge = cv.Canny(img, 150, 250)
-        bluredge = cv.GaussianBlur(edge, (15, 15), 0)
+        img = cv.undistort(img, self.K, self.coeffs)
+
+        img = cv.bilateralFilter(img, 20, 100, 100)
+
+        copy = img.copy()
+
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        edges = cv.Canny(gray, 50, 150, apertureSize=3)
+
         contours, _ = cv.findContours(
-            edge, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        moments = [cv.moments(cnt) for cnt in contours]
-        centroids = [(int(M['m10'] / (M['m00']+1e-5)), int(M['m01'] / (M['m00']+1e-5)))
-                     for M in moments]
-        img2 = img.copy()
-        for c in centroids:
-            cv.circle(img2, c, 5, (0, 0, 255), -1)
-        self.publisher2.publish(CvBridge().cv2_to_imgmsg(img2, 'bgr8'))
+            edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        # combine all contours into one
+        if len(contours) > 0:
+            contours = np.concatenate(contours)
+            cv.drawContours(copy, contours, -1, (0, 255, 0), 3)
+            rect = cv.minAreaRect(contours)
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            centroid = np.mean(box, axis=0)
+            cv.drawContours(copy, [box], 0, (0, 0, 255), 2)
+            cv.circle(copy, (int(centroid[0]), int(
+                centroid[1])), 10, (0, 0, 255), -1)
+            line = cv.fitLine(contours, cv.DIST_L2, 0, 0.01, 0.01)
+            lefty = int((-line[2] * line[1] / line[0]) + line[3])
+            righty = int(((gray.shape[1] - line[2])
+                         * line[1] / line[0]) + line[3])
+            cv.line(copy, (0, lefty),
+                    (gray.shape[1] - 1, righty), (255, 0, 0), 2)
+
+        img_msg = CvBridge().cv2_to_imgmsg(copy, 'bgr8')
+        self.publisher.publish(img_msg)
 
 
 def main(args=None):
