2f546540151c789841fb9ddf809c7380ce58b320
NUC <you@example.com>
2024-04-08 11:27:51 +0200

    add more filter

--- arm/arm/arm_test_detect.py
+++ arm/arm/arm_test_detect.py
@@ -42,7 +42,11 @@ class ImageFilterNode(Node):
         img = CvBridge().imgmsg_to_cv2(msg, 'bgr8')
         img = cv.undistort(img, self.K, self.coeffs)
 
-        img = cv.bilateralFilter(img, 20, 100, 100)
+        cv.imshow('original', img)
+
+        img = cv.GaussianBlur(img, (5, 5), 0)
+        img = cv.bilateralFilter(img, 15, 100, 100)
+        img = cv.medianBlur(img, 5)
 
         copy = img.copy()
 
@@ -50,24 +54,36 @@ class ImageFilterNode(Node):
         edges = cv.Canny(gray, 50, 150, apertureSize=3)
 
         contours, _ = cv.findContours(
-            edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+            edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         # combine all contours into one
         if len(contours) > 0:
-            contours = np.concatenate(contours)
-            cv.drawContours(copy, contours, -1, (0, 255, 0), 3)
-            rect = cv.minAreaRect(contours)
-            box = cv.boxPoints(rect)
-            box = np.int0(box)
-            centroid = np.mean(box, axis=0)
-            cv.drawContours(copy, [box], 0, (0, 0, 255), 2)
-            cv.circle(copy, (int(centroid[0]), int(
-                centroid[1])), 10, (0, 0, 255), -1)
-            line = cv.fitLine(contours, cv.DIST_L2, 0, 0.01, 0.01)
-            lefty = int((-line[2] * line[1] / line[0]) + line[3])
-            righty = int(((gray.shape[1] - line[2])
-                         * line[1] / line[0]) + line[3])
-            cv.line(copy, (0, lefty),
-                    (gray.shape[1] - 1, righty), (255, 0, 0), 2)
+            area_mask = [cv.contourArea(c) > 35 for c in contours]
+            # contours = np.concatenate(contours)
+            # concatenate all contours with area > 5
+            try:
+                contours = np.concatenate(
+                    [c for c, a in zip(contours, area_mask) if a])
+
+                cv.drawContours(copy, contours, -1, (0, 255, 0), 3)
+                rect = cv.minAreaRect(contours)
+                box = cv.boxPoints(rect)
+                box = np.int0(box)
+                centroid = np.mean(box, axis=0)
+                cv.drawContours(copy, [box], 0, (0, 0, 255), 2)
+                cv.circle(copy, (int(centroid[0]), int(
+                    centroid[1])), 10, (0, 0, 255), -1)
+            except:
+                pass
+
+            # line = cv.fitLine(contours, cv.DIST_L2, 0, 0.01, 0.01)
+            # lefty = int((-line[2] * line[1] / line[0]) + line[3])
+            # righty = int(((gray.shape[1] - line[2])
+            #              * line[1] / line[0]) + line[3])
+            # cv.line(copy, (0, lefty),
+            #         (gray.shape[1] - 1, righty), (255, 0, 0), 2)
+
+        cv.imshow('image', copy)
+        cv.waitKey(1)
 
         img_msg = CvBridge().cv2_to_imgmsg(copy, 'bgr8')
         self.publisher.publish(img_msg)
