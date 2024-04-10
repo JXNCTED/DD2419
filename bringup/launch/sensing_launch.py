51dc6c49d2f2d13df9874c86abf897cbbfdbc8f9
NUC <you@example.com>
2024-04-10 17:28:16 +0200

    add laser to scan

--- bringup/launch/sensing_launch.py
+++ bringup/launch/sensing_launch.py
@@ -2,6 +2,7 @@
 
 import launch
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -10,6 +11,26 @@ def generate_launch_description():
             package='odometry', executable='wheel_odom', name='wheel_odom'),
         Node(
             package='odometry', executable='lidar_compensator', name='lidar_compensator',
+        ),
+        Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/camera/depth/color/points']),
+                        ('scan', [LaunchConfiguration(variable_name='scanner'), '/realsense-scan'])],
+            parameters=[{
+                'target_frame': 'cloud',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 1.0,
+                'angle_min': -1.5708,  # -M_PI/2
+                'angle_max': 1.5708,  # M_PI/2
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 4.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
         ),
         Node(
             package='odometry', executable='filter_odom', name='filter_odom'),
@@ -23,4 +44,5 @@ def generate_launch_description():
             package='display_markers', executable='display_markers', name='display_markers'),
         # Node(executable='static_transform_publisher', package='tf2_ros', arguments=[
         #     '--child-frame-id', 'odom', '--frame-id', 'map']),
+
     ])
