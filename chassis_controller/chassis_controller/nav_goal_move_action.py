4c2d331b29a1dac971a97357ffa5bc8a84aa6db5
NUC <you@example.com>
2024-04-28 20:08:54 +0200

    checkout nav_goal_action

---
+++ chassis_controller/chassis_controller/nav_goal_move_action.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from mapping_interfaces.srv import PathPlan
+from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import PoseStamped
+from rclpy.action import ActionClient, ActionServer, GoalResponse, CancelResponse
+from geometry_msgs.msg import Point
+from robp_interfaces.action import Pursuit
+from rclpy.callback_groups import ReentrantCallbackGroup
+from robp_interfaces.action import NavGoal
+
+
+class NavGoalMove(Node):
+    def __init__(self):
+        super().__init__("nav_goal_move")
+        self.cli = self.create_client(PathPlan, "path_plan")
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
+        self.futures = []
+        # request object
+        self.req = PathPlan.Request()
+        self.current_pose = PoseStamped()
+        self.goal_pose = PoseStamped()
+
+        self._cb_group = ReentrantCallbackGroup()
+        self._action_server = ActionServer(
+            self,
+            NavGoal,
+            "nav_goal_move",
+            execute_callback=self.execute_callback,
+            callback_group=self._cb_group,
+            goal_callback=self.goal_callback,
+            cancel_callback=self.cancel_callback,
+        )
+
+        self.path_planned_pub = self.create_publisher(Path, "/path_planned", 10)
+        self.odom_sub_ = self.create_subscription(
+            Odometry, "/odom", self.odom_callback, 10
+        )
+        self.pursuit_client = ActionClient(self, Pursuit, "pursuit")
+        self._loop_rate = self.create_rate(10, self.get_clock())
+
+    def destroy(self):
+        """Cleanup"""
+        self._action_server.destroy()
+        super().destroy_node()
+
+    def goal_callback(self, goal_request):
+        self.goal_pose = goal_request.goal
+        self.get_logger().info("Accepting the goal request")
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, cancel_request):
+        """Accepts the cancel requests"""
+        self.get_logger().info("Accepting cancel request")
+        return CancelResponse.ACCEPT
+
+    def execute_callback(self, goal_handle):
+        self.req.current_pose = self.current_pose
+        self.req.goal_pose = goal_handle.request.goal_pose
+        self.impossible = False
+        self.at_goal = False
+        self.universal_goal_handle = goal_handle
+
+        result = NavGoal.Result()
+        result.done = False
+
+        while not self.impossible:
+            self.stop_pursuit = False
+            future_path = self.cli.call_async(self.req)
+            future_path.add_done_callback(self.path_callback)
+            for _ in range(10):
+                self._loop_rate.sleep()
+            if self.at_goal:
+                result.done = True
+                goal_handle.succeed()
+                break
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                break
+            self.stop_pursuit = True
+            self._loop_rate.sleep()
+
+        return result
+
+    def path_callback(self, future_path):
+        response = future_path.result()
+        if len(response.path.poses) == 0:
+            self.get_logger().warn("Path planning failed")
+            self.impossible = True
+            return
+        self.path_planned_pub.publish(response.path)
+        self.get_logger().info("Path planned published...")
+        waypoints = []
+        for pose in response.path.poses:
+            #TODO: transform wazpoints from map to odom frame
+            waypoints.insert(0, Point(x=pose.pose.position.x, y=pose.pose.position.y))
+        goal_msg = Pursuit.Goal()
+        goal_msg.waypoints = waypoints
+
+        if self.universal_goal_handle.is_cancel_requested:
+            return
+
+        self.pursuit_client.wait_for_server()
+        send_pursuit = self.pursuit_client.send_goal_async(goal_msg)
+        send_pursuit.add_done_callback(self.pursuit_goal_callback)
+
+    def pursuit_goal_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info("Pursuit goal rejected")
+            self.impossible = True
+            return
+        self.get_logger().info("Pursuit goal accepted")
+
+        if self.universal_goal_handle.is_cancel_requested:
+            goal_handle.cancel_goal_async()  # There is a future, I don't care. But perhaps the cancel_request should wait for this to cancel before canceling itself.
+            return
+
+        pursuit_result = goal_handle.get_result_async()
+        pursuit_result.add_done_callback(self.pursuit_result_callback)
+        while rclpy.ok():
+            if self.stop_pursuit:
+                goal_handle.cancel_goal_async()
+                self.stop_pursuit = False
+                break
+            if pursuit_result.done():
+                break
+
+    def pursuit_result_callback(self, future):
+        pursuit_result = future.result()
+        if pursuit_result.success:
+            self.get_logger().info("Pursuit succeeded")
+            self.at_goal = True
+        else:
+            self.get_logger().info("Pursuit failed")
+            self.impossible = True
+
+    def odom_callback(self, msg: Odometry):
+        self.current_pose.pose = msg.pose.pose
+        self.current_pose.header = msg.header
+
+
+def main():
+    rclpy.init()
+
+    executor = rclpy.executors.MultiThreadedExecutor(
+        8
+    )  # TODO check if 3 is the right number
+    nav_goal_move = NavGoalMove()
+    try:
+        rclpy.spin(nav_goal_move, executor=executor)
+    except KeyboardInterrupt:
+        pass
+
+    nav_goal_move.destroy()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- chassis_controller/setup.py
+++ chassis_controller/setup.py
@@ -1,30 +1,31 @@
 from setuptools import find_packages, setup
 
-package_name = 'chassis_controller'
+package_name = "chassis_controller"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='group7',
-    maintainer_email='group7@todo.todo',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="group7",
+    maintainer_email="group7@todo.todo",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'joy_controller = chassis_controller.joy_controller:main',
-            'obj_detect_move = chassis_controller.obj_detect_move:main',
-            'aruco_detect_move = chassis_controller.aruco_detect_move:main',
-            'nav_goal_move = chassis_controller.nav_goal_move:main',
-            'I_want_to_test_the_deep_learning_object_detection_and_the_workspace = chassis_controller.I_want_to_test_the_deep_learning_object_detection_and_the_workspace:main',
+        "console_scripts": [
+            "joy_controller = chassis_controller.joy_controller:main",
+            "obj_detect_move = chassis_controller.obj_detect_move:main",
+            "aruco_detect_move = chassis_controller.aruco_detect_move:main",
+            "arm_detect_object_pick_up = chassis_controller.arm_detect_object_pick_up:main",
+            "nav_goal_move = chassis_controller.nav_goal_move:main",
+            "ngm_simple = chassis_controller.ngm_simple:main",
+            "nav_goal_move_action = chassis_controller.nav_goal_move_action:main",
         ],
     },
 )
